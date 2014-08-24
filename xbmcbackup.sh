36911969e84451792e2f726a62cfb22816056758
hebaishi <hebaishi.husam@gmail.com>
2014-08-24 12:56:53 +0100

    Added stop xbmc command to backup script

--- xbmcbackup.sh
+++ xbmcbackup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /home/pi
-tar cjf xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.bz2 ./xbmc
-mv xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.bz2 /media/Seagate\ Expansion\ Drive/xbmcbackups/
+sudo initctl stop xbmc
+tar czf xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.gz ./xbmc
+mv xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.gz /media/Seagate\ Expansion\ Drive/xbmcbackups/
 sudo sync
