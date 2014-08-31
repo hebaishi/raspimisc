890925f5f611b195598c523636b6f8e4f280271e
hebaishi <hebaishi.husam@gmail.com>
2014-08-31 02:08:45 +0100

    xbmcbackup.sh tested and improved - start xbmc after backup

--- xbmcbackup.sh
+++ xbmcbackup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/pi
 sudo initctl stop xbmc
-tar czf xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.gz ./xbmc
+tar czf xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.gz ./.xbmc
 mv xbmc_userdata_$(date +%d)_$(date +%m)_$(date +%Y).tar.gz /media/Seagate\ Expansion\ Drive/xbmcbackups/
 sudo sync
+sudo initctl start xbmc
